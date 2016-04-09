f285bc9f7824a6ae57c53aa0d303a245995b4104
Francisco Castellanos <fvcastellanos@gmail.com>
2016-04-09 17:54:01 -0600

    Carga inicial

---
+++ ArbolBinario/ArbolBinario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EstructuraDeDatos;
+
+namespace ArbolBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinarioOrdenado abo = new ArbolBinarioOrdenado();
+            abo.Insertar(100);
+            abo.Insertar(50);
+            abo.Insertar(25);
+            abo.Insertar(75);
+            abo.Insertar(150);
+            Console.WriteLine("Impresion entreorden: ");
+            abo.ImprimirEntre();
+            Console.WriteLine("Cantidad de nodos del árbol:" + abo.Cantidad());
+            Console.WriteLine("Cantidad de nodos hoja:" + abo.CantidadNodosHoja());
+            Console.WriteLine("Impresion en entre orden junto al nivel del nodo.");
+            abo.ImprimirEntreConNivel();
+            Console.Write("Artura del arbol:");
+            Console.WriteLine(abo.RetornarAltura());
+            abo.MayorValorl();
+            abo.BorrarMenor();
+            Console.WriteLine("Luego de borrar el menor:");
+            abo.ImprimirEntre();
+            Console.ReadKey();
+        }
+    }
+}
